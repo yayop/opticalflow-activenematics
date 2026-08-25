3d50d0dc911a3c80e543b588f9035e4a89798c6f
yayop <77552656+yayop@users.noreply.github.com>
2026-08-25 14:52:40 +0200

    Expose zlib for verification on swift login

--- shell.nix
+++ shell.nix
@@ -8,10 +8,11 @@ pkgs.mkShell {
     pkgs.python311
     pkgs.python311Packages.virtualenv
     pkgs.git
+    pkgs.zlib
   ];
 
   shellHook = ''
     export MPLBACKEND=Agg
-    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib ]}:/run/opengl-driver/lib:''${LD_LIBRARY_PATH:-}"
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib pkgs.zlib ]}:/run/opengl-driver/lib:''${LD_LIBRARY_PATH:-}"
   '';
 }
